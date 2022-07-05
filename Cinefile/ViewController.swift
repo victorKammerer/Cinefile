6ad65bb6f470a21a438b0431f1f875a3e8a3052b
Victor Kammerer <vko@cin.ufpe.br>
2022-07-05 08:42:05 -0300

    Aplicativo para classificar filmes

---
+++ Cinefile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinefile
+//
+//  Created by vko on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
